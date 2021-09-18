4da242ca8bc441c2e9210441e8462bb178714d87
rahul-singh-sandhu <90906486+rahul-singh-sandhu@users.noreply.github.com>
2021-09-18 13:56:38 +0100

    I managed to get connection choice working! I will explain in the lesson later today.

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,15 @@
 import ftplib
 from ftplib import FTP
 
-connectionChoice = input("Would you like to use a hostname or an IP address?")
+connectionChoice = input("Would you like to use a hostname or an IP address? \n")
 if connectionChoice == "hostname":
-    ip = input("Please enter an hostname: \n")
+    hostname = input("Please enter an hostname: \n")
+    user = input("Please enter a username: \n")
+    passwd = input("Please enter a password: \n")
+    ftp = FTP(hostname)
+    ftp.login(user=user, passwd=passwd)
+    print("File List: ")
+    files = ftp.dir()
+    print(files)
 elif connectionChoice == "IP":
     ip = int(input("Please enter an IP address: \n"))
-
-ftp = FTP('ftp.dlptest.com')
-ftp.login(user='dlpuser', passwd ='rNrKYTX9g7z3RgJRmxWuGHbeu')
-print("File List: ")
-files = ftp.dir()
-print(files)
