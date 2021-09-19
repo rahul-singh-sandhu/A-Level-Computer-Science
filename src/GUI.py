83fdacb44187ff79dee37f5a76dab585d0111c99
rahul-singh-sandhu <90906486+rahul-singh-sandhu@users.noreply.github.com>
2021-09-19 20:18:09 +0100

    This is the completed CLI!

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,30 @@
 from tkinter import *
 from tkinter import ttk
+import ftplib
+from ftplib import FTP
 window = Tk()
 window.wm_title("Networking Client")
 window.resizable(True,True)
 
 
 def storeIP():
-    iphost = ip_hostname.get()
-    Label(window, text=f'{iphost}, Registered!', pady=20, bg='#ffbf00').pack()
+    ip_host = ip_hostname.get()
+    user_login = user.get()
+    passwd_login = passwd.get()
+    Label(window, text=f'{ip_host}, submitted!', pady=20, bg='#ffbf00').pack()
+    ftp = FTP(ip_host)
+    ftp.login(user=user_login, passwd=passwd_login)
+    print("File List: ")
+    files = ftp.dir()
+    print(files)
 
 ip_hostname = Entry(window)
 ip_hostname.pack(pady=30)
+user = Entry(window)
+user.pack(pady=30)
+passwd = Entry(window)
+passwd.pack(pady=30)
+
 
 Button(
     window,
@@ -18,6 +32,5 @@ Button(
     padx=10,
     pady=5,
     command=storeIP
-    ).pack
-
+    ).pack()
 window.mainloop()
