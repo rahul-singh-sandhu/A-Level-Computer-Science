5466e6c00555f8edbb0d87be2e6ef87b6465a7ef
rahul-singh-sandhu <90906486+rahul-singh-sandhu@users.noreply.github.com>
2021-09-20 15:38:19 +0100

    Commented and upload button added

--- src/GUI.py
+++ src/GUI.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import ftplib
 from ftplib import FTP
+from tkinter import filedialog
 
 #This function creates the tkinter windows.
 import background
@@ -12,7 +13,7 @@ window = Tk()
 window.wm_title("Networking Client")
 window.resizable(True,True)
 window.configure(bg='white')
-window.geometry("200x300")
+window.geometry("200x430")
 
 
 #This function is the function that retrives the hostname, username and password that the user enters, and then returns the list of files on the ftp server.
@@ -21,24 +22,44 @@ def storeIP():
     ip_host = ip_hostname.get()
     user_login = user.get()
     passwd_login = passwd.get()
-    #This prints the submission message.
-    Label(window, text=f'{ip_host} submitted!', pady=20, bg='lightgray').pack()
+    directory_path = directory.get()
     #This function loads the FTP server.
     ftp = FTP(ip_host)
     ftp.login(user=user_login, passwd=passwd_login)
+    ftp.cwd(directory_path)
     #This function prints the directory structure.
     print("File List: ")
     files = ftp.dir()
     print(files)
 def upload():
+    filename = data.get()
     ip_host = ip_hostname.get()
     user_login = user.get()
     passwd_login = passwd.get()
     ftp = ftplib.FTP(ip_host, user_login, passwd_login)
-    file = open('Testdoc.rtf', 'rb')  # file to send
-    ftp.storbinary('STOR Testdoc.rtf', file)  # send the file
+    with open(filename, "rb") as file:
+        ftp.storbinary(f"STOR {filename}", file)
     print("File List: ")
     files = ftp.dir()
+    print(files)
+def download():
+    filename = data.get()
+    ip_host = ip_hostname.get()
+    user_login = user.get()
+    passwd_login = passwd.get()
+    ftp = ftplib.FTP(ip_host, user_login, passwd_login)
+    with open(filename, "wb") as file:
+        ftp.retrbinary(f"RETR {filename}", file.write)
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("Text files",
+                                                      "*.txt*"),
+                                                     ("all files",
+                                                      "*.*")))
+
+    # Change label contents
+    label_file_explorer.configure(text="File Opened: " + filename)
 #This code displays the form
 ip_hostname = Entry(window)
 ip_hostname.pack(anchor='w',padx=10,pady=30)
@@ -49,13 +70,32 @@ user.insert(END, 'Username')
 passwd = Entry(window)
 passwd.pack(anchor='w',padx=10,pady=30)
 passwd.insert(END, 'Password')
+data = Entry(window)
+data.pack(anchor='w', padx=10, pady=30)
+data.insert(END, 'File Names')
+directory = Entry(window)
+directory.pack(anchor='w',padx=10,pady=30)
+directory.insert(END, 'Directory')
+label_file_explorer = Label(window,
+                            text="File Explorer using Tkinter",
+                            width=100, height=4,
+                            fg="blue")
 
+Button(
+    window,
+    text="Browse",
+    padx=10,
+    pady=5,
+    width=10,
+    command=browseFiles
+    ).pack(anchor='w', padx=10)
 #This code is for the submit button. It utilises the storeIP() function
 Button(
     window,
     text="Submit",
     padx=10,
     pady=5,
+    width=10,
     command=storeIP
     ).pack(anchor='w', padx=10)
 Button(
@@ -63,6 +103,16 @@ Button(
     text="Upload",
     padx=10,
     pady=5,
+    width=10,
     command=upload
     ).pack(anchor='w', padx=10)
+Button(
+    window,
+    text="Download",
+    padx=10,
+    pady=5,
+    width=10,
+    command=download
+    ).pack(anchor='w', padx=10)
+
 window.mainloop()
